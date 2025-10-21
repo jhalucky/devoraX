70a22a05bf11ea94740c2170f5e14f30a0accc8c
Lucky Jha <jhalucky61@gmail.com>
2025-10-21 09:01:07 +0530

    clerk routing

--- src/app/sign-up/[[...rest]]/page.tsx
+++ src/app/sign-up/[[...rest]]/page.tsx
@@ -5,11 +5,13 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-    <SignUp
-      path="/sign-up"
-      routing="path"
-      signInUrl="/sign-in"
-    />
+      <div className="scale-90 sm:scale-75 md:scale-80 lg:scale-90 transform">
+        <SignUp
+          path="/sign-up"
+          routing="path"
+          signInUrl="/sign-in"
+        />
+      </div>
     </div>
   );
 }
